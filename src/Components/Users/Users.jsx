71c73d77177357701d418215c18cf8de49c6d927
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-10-04 16:14:36 +0200

    Load data on route using loader and useLoaderData

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,10 +1,21 @@
+import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
+import './Users.css';
+
 
 
 const Users = () => {
+    const users = useLoaderData();
+    console.log(users);
     return (
         <div>
-            <h2>our users</h2>
+            <h2>our users : {users.length}</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, nesciunt.</p>
+            <div className="users">
+                {
+                    users.map(user => <User key={user.id} user={user}></User>)
+                }
+            </div>
 
         </div>
     );
