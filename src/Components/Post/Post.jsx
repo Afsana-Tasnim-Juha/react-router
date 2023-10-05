92325985efbea396a8d991ff7de4e46d16ef426d
Afsana-Tasnim-Juha <111423065+Afsana-Tasnim-Juha@users.noreply.github.com>
2023-10-05 11:42:33 +0200

    Navigate on Click, useNavigate

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,19 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Post = ({ post }) => {
     const { id, title } = post;
+    const navigate = useNavigate();
+
 
     const postStyle = {
         border: '2px solid green',
         padding: '5px',
         borderRadius: '20px'
     }
+
+    const handleShowDetail = () => {
+        navigate(`/post/${id}`);
+
+
+
+    }
     return (
         <div style={postStyle} >
             <h4>Post of id {id}</h4>
             <p>{title}</p>
-            <Link to={`/post/${id}`}>Post Details</Link>
-            <Link to={`/post/${id}`}><button>Dhow Details</button></Link>
+            <Link to={`/ post / ${id}`}>Post Details</Link>
+            <Link to={`/ post / ${id}`}><button>Dhow Details</button></Link>
+            <button onClick={handleShowDetail}>Click to see details</button>
 
 
         </div >
